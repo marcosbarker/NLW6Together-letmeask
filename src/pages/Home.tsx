2becbf9ff7b61b45db2a6110515aa4e541204299
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-06-24 04:05:01 -0300

    code: add lesson2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,11 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss';
+
 export function Home(){
   return(
-    <div>
+    <div id=page-auth>
       <aside>
         <img src={illustrationImg} alt=""/>
         <strong>Cria salas de Q&amp;A ao-vivo</strong>
@@ -34,5 +36,4 @@ export function Home(){
     </div>
 
   )
-
 }
